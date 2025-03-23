5baefcb9bc728b2f9c5cd8f08108fd74ca82356a
sayan4545 <sayanchatterjee3010@gmail.com>
2025-03-24 00:32:02 +0530

    added spring security dependency

---
+++ src/test/java/com/devsayan/springsecurity/SpringSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsayan.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
